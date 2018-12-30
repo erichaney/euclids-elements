68c563187423140a1c6f37f21aa2588983d35781
Eric Haney <ehaney@outlook.com>
2018-12-30 09:30:58 -0700

    Add prop1-5 diagram

---
+++ book1/prop1-5-diagram.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(require metapict
+         "../metapict-euclid.rkt")
+
+(define A (pt 0 0.8))
+(define B (pt -0.4 -.2))
+(define C (pt 0.4 -.2))
+(define D (med 1.8 A B))
+(define E (med 1.8 A C))
+(define F (med 0.6 B D))
+(define G (med 0.6 C E))
+
+
+(define prop1-5-diagram
+  (draw
+   ;given
+   (curve A -- B -- C -- cycle)
+   (point-label "A" A (top))
+   (point-label "B" B (lft))
+   (point-label "C" C (rt))
+   ;1
+   (curve B -- D)
+   (curve C -- E)
+   (point-label "D" D (bot))
+   (point-label "E" E (bot))
+   ;2
+   (point-dot-label "F" F (lft))
+   ;3
+   (point-dot-label "G" G (rt))
+   ;4
+   (curve B -- G)
+   (curve F -- C)))
+
+
+(penwidth 2 prop1-5-diagram)
+;(send (pict->bitmap (penwidth 2 prop1-5-diagram)) save-file "prop1-5-diagram.png" 'png)
