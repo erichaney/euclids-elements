d1682cd1485a311b5acfecb0e97557561ff67d5f
Eric Haney <ehaney@outlook.com>
2018-12-30 16:43:33 -0700

    Add prop1-9 and prop1-10 diagrams

---
+++ book1/prop1-10-diagram.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require metapict
+         "../metapict-euclid.rkt")
+
+(def A (pt -.5 .5))
+(def B (pt+ A (vec -.2 -.6)))
+(def C (pt+ A (vec .3 -.4)))
+(def v (vec 1 0))
+(defv (D E F)
+  (values (pt+ A v)
+          (pt+ B v)
+          (pt+ C v)))
+(def G (pt+ D (vec .2 -.02)))
+
+
+(define prop1-8-diagram
+  (draw
+   ; given
+   (curve A -- B -- C -- cycle)
+   (point-label "A" A (top))
+   (point-label "B" B (llft))
+   (point-label "C" C (rt))
+   (curve D -- E -- F -- cycle)
+   (point-label "D" D (top))
+   (point-label "E" E (llft))
+   (point-label "F" F (rt))
+   ; 1
+   (curve  E -- G -- F)
+   (point-label "G" G (top))))
+
+(penwidth 2 prop1-8-diagram)
+;(send (pict->bitmap (penwidth 2 prop1-8-diagram)) save-file "prop1-8-diagram.png" 'png)
---
+++ book1/prop1-9-diagram.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require metapict
+         "../metapict-euclid.rkt")
+
+(def A (pt 0 .7))
+(def B (pt -.5 -.6))
+(def C (pt .5 -.6))
+(def D (med 0.6 A B))
+(def E (med 0.6 A C))
+(def F (cadr (intersect-cc D (dist D E) E (dist D E))))
+
+(define prop1-9-diagram
+  (draw
+   ; given
+   (curve C -- A -- B)
+   (point-label "A" A (top))
+   (point-label "B" B (llft))
+   (point-label "C" C (lrt))
+   ; 1
+   (point-dot-label "D" D (ulft))
+   ; 2
+   (point-dot-label "E" E (urt))
+   ; 3
+   (curve D -- E)
+   ; 4
+   (curve D -- F -- E)
+   (point-label "F" F (bot))
+   ; 5
+   (curve A -- F)))
+   
+(penwidth 2 prop1-9-diagram)
+;(send (pict->bitmap (penwidth 2 prop1-9-diagram)) save-file "prop1-9-diagram.png" 'png)
