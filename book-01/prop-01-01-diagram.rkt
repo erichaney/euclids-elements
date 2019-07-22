29b7d7d5396911322f3523aa8c06bdd1f6c95ca8
Eric Haney <ehaney@outlook.com>
2019-07-21 21:01:24 -0700

    Staged diagrams for prop 1

--- book-01/prop-01-01-diagram.rkt
+++ book-01/prop-01-01-diagram.rkt
@@ -10,24 +10,40 @@
 (define D (pt+ A (vec@ (dist A B) pi)))
 (define E (pt+ B (vec@ (dist A B) 0)))
 
-(define prop-01-01-diagram
+(define diagram-0
   (draw
    ; given
    (curve A -- B)
    (point-label "A" A (lft))
-   (point-label "B" B (rt))
-   ; 1
+   (point-label "B" B (rt))))
+
+(define diagram-1
+  (draw
+   diagram-0
    (circle A B)
    (point-label "C" C (top))
-   (point-label "D" D (lft))
-   ; 2
+   (point-label "D" D (lft))))
+
+(define diagram-2
+   (draw
+    diagram-1
    (circle B A)
-   (point-label "E" E (rt))
-   ; 3
+   (point-label "E" E (rt))))
+
+(define diagram-3
+   (draw
+    diagram-2
    (curve A -- C)
    (curve B -- C)))
 
-;(penwidth 2 prop-01-01-diagram)
+(define (prop-01-01-diagram [n 3])
+  (case n
+    [(0) diagram-0]
+    [(1) diagram-1]
+    [(2) diagram-2]
+    [else diagram-3]))
+
+(penwidth 2 (prop-01-01-diagram))
 ;(send (pict->bitmap prop-01-01-diagram) save-file "prop-01-01-diagram.png" 'png)
 
 
